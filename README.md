dc5edeb3f13ef318654a76db5112f5d7bb61af7b
Silmara-Basso <silmarabasso@gmail.com>
2025-07-31 00:15:11 -0300

    :tada: Commit full

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pipeline-mlops-aws
 Deploy  do  modelo  de  Machine  Learning através de API em nuvem AWS
+
+Estes scripts vão servir apenas como base para apoiar a configuração AWS
+
+![Solução.](/images/overview.png)
+
+
+- Criar o buket "pinguins-890582101704" e carregar o dataset.csv
+- Abrir o Sagemaker studio e carregar o script ipynb no Jupyter e executar os scripts para salvar o modelo no S3
+- Configurar a politica de acesso criando um perfil com os acessos:
+    - AWSLambdaBasicExecutionRole
+    - AmazonSageMakerFullAccess
+- Criar a função Lambda com o codigo do script lambda_function.py
+- Criar REST API com API Gateway e no método configurar o POST para a função Lambda
+
+### Obs: Ainda quero automatizar isto tudo com IAC 
---
+++ lambda_function.py
@@ -0,0 +1,54 @@
+# Deploy com SageMaker, Lambda e API Gateway
+
+# Imports
+import boto3
+import json
+
+# Função lambda
+def lambda_handler(event, context):
+
+    # Cria o cliente SageMaker
+    runtime_client = boto3.client('runtime.sagemaker')
+
+    # Nome do endpoint
+    endpoint_name = 'sagemaker-xgboost-2025-07-30-23-57-07-172'
+
+    # Variáveis de entrada
+    x1 = event['x1']
+    x2 = event['x2']
+    x3 = event['x3']
+    x4 = event['x4']
+
+    # Cria a amostra de dados
+    sample = '{},{},{},{}'.format(x1, x2, x3, x4)
+
+    # Invoka o endpoint, passando a amostra como entrada para receber como resposta a previsão
+    response = runtime_client.invoke_endpoint(EndpointName = endpoint_name,
+                                              ContentType = 'text/csv',
+                                              Body = sample)
+    
+    # Formata a resposta
+    result = int(float(response['Body'].read().decode('ascii')))
+
+    print(result)
+
+    # Retorno
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'Prediction': result})
+    }
+
+
+# Exemplo para testar a função lambda
+
+"""
+
+{
+  "x1": "32.1",
+  "x2": "15.5",
+  "x3": "188.0",
+  "x4": "3050.0"
+}
+
+"""
+
